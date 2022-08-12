e2df09ab53ff4bf7494ca73ab61166d1f18cff61
kaye-fullstack <kaye@fullstack.ph>
2022-08-12 10:13:54 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ git checkout --orphan gh-pages
 git add -f dist
 git commit -m "Rebuild GitHub pages"
 git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages
-git checkout master
+git checkout develop
